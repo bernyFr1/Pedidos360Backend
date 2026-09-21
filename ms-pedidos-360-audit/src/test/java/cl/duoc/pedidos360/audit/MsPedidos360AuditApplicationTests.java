0c8b72925009b543f033f3135c320caf92a1fe59
bernyFr1 <be.floresr@duocuc.cl>
2026-09-21 20:17:45 -0300

    feat: estructura inicial del proyecto y microservicios

---
+++ ms-pedidos-360-audit/src/test/java/cl/duoc/pedidos360/audit/MsPedidos360AuditApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.duoc.pedidos360.audit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsPedidos360AuditApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ ms-pedidos-360-notify/src/test/java/cl/duoc/pedidos360/notify/MsPedidos360NotifyApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.duoc.pedidos360.notify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsPedidos360NotifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
